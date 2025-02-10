9a665841dd95531af6c5123bcf84256346dbfe9d
Rqmba <ramblack45@gmail.com>
2025-02-10 14:32:26 +0100

    create register user, save user to firestore, user sign in, alerts with React toastify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
+import {toast} from 'react-toastify'
 import {Link, useNavigate} from "react-router-dom"
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import {setDoc, doc, serverTimestamp  } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import  {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibiltyIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +25,33 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword
+      (auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      toast.error('Something went wrong with registration')
+    }
+  }
+
     return (
       <>
           <div className="pageContainer">
@@ -29,13 +60,13 @@ function SignUp() {
             </header>
 
             <main>
-              <form>
+              <form onSubmit={onSubmit}>
               <input  
                 type="text"
                 className="nameInput"
                 placeholder="Name"
                 id="name"
-                value={email}
+                value={name}
                 onChange={onChange}
                 />
                 <input  
