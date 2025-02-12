132fd885318235703e7573182ef85ff55eaf3dd6
Rqmba <ramblack45@gmail.com>
2025-02-12 14:21:17 +0100

    Create, Alert with toastify, user Logout, display & update user details, privateRoute Component & useAuthStatus hook, forgot password, google oauth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import {Link, useNavigate} from "react-router-dom"
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 import {setDoc, doc, serverTimestamp  } from 'firebase/firestore'
 import { db } from '../firebase.config'
+import OAuth from "../components/OAuth"
 import  {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibiltyIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -112,7 +113,7 @@ function SignUp() {
                 </div>  
               </form>
 
-              {/* Google OAuth */}
+              <OAuth />
 
               <Link to='/sign-in' className="registerLink">
                 Sign Up Instead
